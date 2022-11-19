2458b99925a074dd5a70c8e9326b99d00dac6428
melig99 <56968146+melig99@users.noreply.github.com>
2022-11-19 01:31:18 -0300

    Actualizacion servicio punto

--- src/componentes/servicio/consultar_punto_ por_monto.js
+++ src/componentes/servicio/consultar_punto_ por_monto.js
@@ -2,9 +2,26 @@ import React,{useState,useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import Peticiones from '../../helpers/peticiones';
 
 const ConsultarMonto = () => {
     const [estadoForm,setEstadoForm] = useState(true);
+    const [datosForm,setDatosForm] = useState({});
+    const [obtenerPanel,,] = Peticiones();
+    const [monto,setDatosMonto] = useState({puntaje_asignado: 0});
+
+    const guardarDatos=(objeto)=>{
+        let temp = {...datosForm};
+        temp[objeto.target.id]=objeto.target.value;
+        setDatosForm(temp);
+        console.log(temp);
+
+    }
+
+    const enviarForm = ()=>{
+        obtenerPanel('reporte/reglaspunto',setDatosMonto,datosForm.monto);
+
+    }
     return (
     <>
         <Modal show={estadoForm}  animation={false} onHide={()=>setEstadoForm(!estadoForm)}>
@@ -15,7 +32,7 @@ const ConsultarMonto = () => {
                 <div className="container">
                     <div className="row" >
                         <Form.Label htmlFor="monto">Monto </Form.Label>
-                        <Form.Control type="number" min="0" id="monto" />
+                        <Form.Control type="number" min="0" id="monto" onChange={(e)=>{guardarDatos(e)}} />
                     </div> 
                 </div>
             </Modal.Body>
@@ -23,13 +40,13 @@ const ConsultarMonto = () => {
                 <div className="container">
                     <div className="row" >
                         {/* <Form.Label htmlFor="cantidad_puntos">Cantidad de puntos que equivale </Form.Label> */}
-                        <div class="alert alert-primary bi bi-info-circle-fill" role="alert">  La cantidad de puntos es: </div>
+                        <div className="alert alert-primary bi bi-info-circle-fill" role="alert">  La cantidad de puntos es: {monto.puntaje_asignado} </div>
                     </div> 
                 </div>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={()=>setEstadoForm(!estadoForm)} >Cerrar</Button>
-                <Button variant="success" > Enviar</Button>
+                <Button variant="success" onClick={()=>enviarForm()}> Enviar</Button>
             </Modal.Footer>
         </Modal>
     </>
