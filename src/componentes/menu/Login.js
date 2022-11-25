6a1179dc4955ec17d9018c2fc39f5a0205719457
melig99 <56968146+melig99@users.noreply.github.com>
2022-11-25 00:23:43 -0300

    Creacion de componente login

---
+++ src/componentes/menu/Login.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Form, Button, Card, Alert, Container } from "react-bootstrap"
+import { Link } from "react-router-dom";
+
+
+const Login = () =>{
+    return (
+        <>
+            <Container  className="d-flex align-items-center justify-content-center"  style={{height: "100vh",maxWidth: "100vw",padding: '0', backgroundColor: "#212529",  margin: "0"}}>
+                <Card>
+                    <Card.Body >
+                    <h2 className="text-center mb-4">Inicio Sesion</h2>
+                    {/* {error && <Alert variant="danger">{error}</Alert>} */}
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Correo Electronico</Form.Label>
+                            <Form.Control type="email"/>
+                        </Form.Group>
+                        <br/>
+                        <Form.Group id="password">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password"  />
+                        </Form.Group>
+                        <br/>
+                        <Link to="/"><Button  className="w-100" type="submit">Iniciar</Button></Link>
+                    </Form>
+                        <div className="w-100 text-center mt-3">
+                        {/* <Link to="/forgot-password">Forgot Password?</Link> */}
+                        </div>
+                    </Card.Body>
+                </Card>
+                
+            </Container>
+        </>
+    );
+  }
+  
+  export default Login
